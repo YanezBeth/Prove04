6ce547378423781f59c339d5fee341208860a86f
E Brent Nelson <ebrentnelson@gmail.com>
2022-01-29 21:32:47 -0700

    Edit Beth's file

--- HelloFromBeth/src/yanez/Main.java
+++ HelloFromBeth/src/yanez/Main.java
@@ -2,7 +2,10 @@ package yanez;
 
 public class Main {
 
+    private static final String name = "Beth";
+
     public static void main(String[] args) {
-        System.out.println("Hello, my name is Beth.");
+        System.out.println(String.format("Hello, my name is %s.", name));
+        System.out.println(String.format("Hello %s!", name));
     }
 }
